702b9bd5fbca4b0c35955ba28907a2b5e292a020
OriCat101 <redbadcommander@protonmail.ch>
2024-02-07 18:05:11 +0100

    added more meta attributes, upload is a is a option, album now gets sorted

--- analyzer.py
+++ analyzer.py
@@ -1,14 +1,11 @@
 import base64
 import os
 import tempfile
-import webbrowser
-
-import flickrapi
 import requests
-from pydub import AudioSegment
 import matplotlib.pyplot as plt
-from mutagen.flac import FLAC
 import hashlib
+from pydub import AudioSegment
+from mutagen.flac import FLAC
 from tqdm import tqdm
 
 
@@ -17,7 +14,7 @@ def get_audio_codec(file_path):
   return extension.lower()[1:]  # Removing the dot at the beginning
 
 
-def save_spectrogram_plot(audio, output_folder, file_path):
+def save_spectrogram_plot(audio, file_path):
   mixed_channel = audio.set_channels(1)
 
   plt.specgram(
@@ -31,51 +28,67 @@ def save_spectrogram_plot(audio, output_folder, file_path):
   plt.xlabel('Time (s)')
   plt.ylabel('Frequency (Hz)')
 
-  # Create a temporary directory if it doesn't exist
   tmp_folder = tempfile.mkdtemp()
-
-  # Save the spectrogram plot to a file in the temporary directory
-  output_file = os.path.join(tmp_folder,
+  spectrogram = os.path.join(tmp_folder,
                              f"{os.path.splitext(os.path.basename(file_path))[0]}_mixed_channel_spectrogram.png")
-  plt.savefig(output_file)
+  plt.savefig(spectrogram)
   plt.close()
 
-  # Return the path to the saved spectrogram plot
-  return output_file
+  return spectrogram
 
 
-def analyze_album(album_folder, output_folder="output/spectrogram"):
+def analyze_album(album_folder, upload_spectrogram=False):
   flac_files = [file for file in os.listdir(album_folder) if file.lower().endswith('.flac')]
   album = {}
 
   for file in tqdm(flac_files, desc="Processing tracks", unit="files"):
     file_path = os.path.join(album_folder, file)
-    flac_info = get_flac_info(file_path, output_folder)
+    flac_info = get_flac_info(file_path, upload_spectrogram)
     file_name = os.path.basename(file_path)
     album[file_name] = flac_info
+
+    try:
+      album = dict(sorted(album.items(), key=lambda x: int(x[1]['#']), reverse=False))
+    except:
+      pass
 
   return album
 
 
-def get_flac_info(file_path, output_folder="output/spectrogram"):
+def get_flac_info(file_path, upload_spectrogram=False):
   result = {}
 
   if os.path.isfile(file_path) and file_path.lower().endswith('.flac'):
-    audioSegment = AudioSegment.from_file(file_path, format="flac")
-    plot_path = save_spectrogram_plot(audioSegment, output_folder, file_path)
 
     audio = FLAC(file_path)
 
-    # Extract other parameters
+    if 'tracknumber' in audio:
+      result['#'] = audio['tracknumber'][0]
+    else:
+      result['#'] = "-"
+
+    if 'artist' in audio:
+      result['artist'] = audio['artist'][0]
+    else:
+      result['artist'] = "Unknown"
+
+    if 'title' in audio:
+      result['title'] = audio['title'][0]
+    else:
+      result['title'] = "Unknown"
+
     result['duration'] = convert_seconds_to_hms(audio.info.length)
     result['channels'] = audio.info.channels
     result['bits_per_sample'] = audio.info.bits_per_sample
     result['bitrate'] = f"{int(audio.info.bitrate / 1000)} kbps"
     result['codec'] = "FLAC"
     result['embedded_cuesheet'] = audio.cuesheet
-    result['spectrogram'] = f"[url]{upload_image(plot_path)}[/url]"
 
-    # Calculate MD5 hash of audio data
+    if upload_spectrogram:
+      audio_segment = AudioSegment.from_file(file_path, format="flac")
+      plot_path = save_spectrogram_plot(audio_segment, file_path)
+      result['spectrogram'] = f"[url]{upload_image(plot_path)}[/url]"
+
     with open(file_path, 'rb') as flac_file:
       audio_data = flac_file.read()
       md5_hash = hashlib.md5(audio_data).hexdigest()
@@ -90,23 +103,21 @@ def get_flac_info(file_path, output_folder="output/spectrogram"):
 def convert_seconds_to_hms(seconds):
   hours, remainder = divmod(seconds, 3600)
   minutes, seconds = divmod(remainder, 60)
-  return f"{int(hours)}:{int(minutes)}:{int(seconds)}"
+
+  return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
 
 
 def generate_bbcode_table(metadata):
   bbcode = "[table]\n"
-
-  # Header row
   bbcode += "[tr]\n"
   bbcode += f"[td][b]Filename[/b][/td]\n"
+
   for key in metadata[list(metadata.keys())[0]]:
     bbcode += f"[td][b]{key.capitalize()}[/b][/td]\n"
   bbcode += "[/tr]\n"
 
-  # Data rows
   for track, track_info in metadata.items():
     bbcode += f"[tr]\n[td]{track}[/td]\n"
-
     for value in track_info.values():
       bbcode += f"[td]{value}[/td]\n"
 
@@ -117,26 +128,20 @@ def generate_bbcode_table(metadata):
 
 
 def upload_image(file_path):
-  # API endpoint
   api_url = "https://freeimage.host/api/1/upload"
 
-  # Read the image file
   with open(file_path, "rb") as file:
-    # Encode the image as base64
     base64_image = base64.b64encode(file.read()).decode("utf-8")
 
-  # API parameters
+  # API
   params = {
     "key": "6d207e02198a847aa98d0a2a901485a5",
     "action": "upload",
     "source": base64_image,
     "format": "json"
   }
-
-  # Make the API request
   response = requests.post(api_url, data=params)
 
-  # Check if the request was successful
   if response.status_code == 200:
     result = response.json()
     return result["image"]["url"]
@@ -145,8 +150,6 @@ def upload_image(file_path):
 
 
 if __name__ == "__main__":
-  # Example usage:
   album_folder = r"%userprofile%\Downloads\spam\Petar Dundov - At The Turn Of Equilibrium (2016)(FLAC)(CD)"
-  output_folder = "output/folder1"
-  album_data = analyze_album(album_folder, output_folder)
+  album_data = analyze_album(album_folder)
   print(generate_bbcode_table(album_data))
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import analyzer
 def main():
     album_path=input("Album folder: ")
-    album_meta=analyzer.analyze_album(album_path)
+    upload_spectrogram = input("Upload spectrogram? (y/n, default=n): ")
+    if upload_spectrogram.lower() == "y":
+        upload_spectrogram = True
+    else:
+        upload_spectrogram = False
+
+    album_meta=analyzer.analyze_album(album_path, upload_spectrogram)
     print(analyzer.generate_bbcode_table(album_meta))
+    # print(album_meta)
 
 if __name__ == "__main__":
     main()
